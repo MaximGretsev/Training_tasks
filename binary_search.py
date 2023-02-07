7ea2320a39bcf7ee338467a9bf931285156860a2
Maxim Gretsev <98987566+MaximGretsev@users.noreply.github.com>
2023-02-07 11:11:12 +0300

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -14,7 +14,3 @@ class BinarySearch:
             elif middle_value > elem:
                 right_border = middle_index - 1
         return f'Element {elem} not fount'
-
-# cls = BinarySearch()
-# sorted_list = sorted([random.randint(0, 55) for _ in range(20)])
-# print(cls.binary_search(34, sorted_list), sorted_list)
